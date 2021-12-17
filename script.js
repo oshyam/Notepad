bec56c15b9c7c36710d2fa05e25b9483abe6272e
Sam <myworkplaceservice@gmail.com>
2021-12-17 21:50:36 +0530

    notepad app

---
+++ script.js
@@ -0,0 +1,30 @@
+function notepadText(){
+    var notepadVal = document.getElementById('miniNote').value;
+    var textLength = notepadVal.length;
+    alert("Hey! Your Text Length is "+ textLength + " and text is: "+ notepadVal);
+}
+
+function saveTextAsFile(textToWrite, fileNameToSaveAs)
+{
+    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'}); 
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = "Download File";
+    if (window.webkitURL != null)
+    {
+        // Chrome allows the link to be clicked
+        // without actually adding it to the DOM.
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    }
+    else
+    {
+        // Firefox requires the link to be added to the DOM
+        // before it can be clicked.
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
+}
